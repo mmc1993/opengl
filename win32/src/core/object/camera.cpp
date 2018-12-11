7cc4ced751e20713b16442a1ebde020f1b8bff12
mmc1206 <1257335379@qq.com>
2018-12-12 02:58:18 +0800

    研究矩阵乘法 研究四元数 待实现Transform 待实现Camera 待实现Sprite 待完善Render

--- win32/src/core/object/camera.cpp
+++ win32/src/core/object/camera.cpp
@@ -1,6 +1,7 @@
 #include "camera.h"
 
 Camera::Camera()
+    : _isChange(true)
 {
 }
 
@@ -10,6 +11,32 @@ Camera::~Camera()
 
 void Camera::Init(float fov, float wdivh, float near, float far)
 {
-    //  append transform
+    _project = glm::perspective(fov, wdivh, near, far);
+}
+
+void Camera::LookAt(const glm::vec3 & eye, const glm::vec3 & pos, const glm::vec3 & up)
+{
+    _view = glm::lookAt(eye, pos, up);
+
+    _transform.Translate(eye);
+}
+
+const glm::vec3 & Camera::GetUp() const
+{
+    return _up;
+}
+
+const glm::vec3 & Camera::GetEye() const
+{
+    return _eye;
+}
+
+const glm::vec3 & Camera::GetPos() const
+{
+    return _pos;
+}
+
+void Camera::Apply()
+{
 }
 
--- win32/src/core/object/camera.h
+++ win32/src/core/object/camera.h
@@ -3,12 +3,24 @@
 #include "../include.h"
 #include "../math/vec4.h"
 #include "../math/matrix.h"
+#include "../component/transform.h"
 #include "object.h"
 
 class Camera: public Object {
 public:
     Camera();
     ~Camera();
-
     void Init(float fov, float wdivh, float near, float far);
+    void LookAt(const glm::vec3 & eye, const glm::vec3 & pos, const glm::vec3 & up);
+    const glm::vec3 & GetUp() const;
+    const glm::vec3 & GetEye() const;
+    const glm::vec3 & GetPos() const;
+    void Apply();
+
+private:
+    bool _isChange;
+    glm::mat4 _view;
+    glm::mat4 _project;
+    glm::vec3 _eye, _pos, _up;
+    Transform _transform;
 };
