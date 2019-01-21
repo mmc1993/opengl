e3406b6c978957768bbf9268462899bcc2641fd4
mmc <1257335379@qq.com>
2019-01-21 22:55:50 +0800

    多光源
    片段着色器实现

--- win32/res/shader/1.shader.fs
+++ win32/res/shader/1.shader.fs
@@ -6,11 +6,41 @@ uniform mat4 mvp_;
 uniform mat4 mv_;
 uniform mat4 m_;
 
-uniform struct Light_ {
+struct LightDirect_ {
+	vec3 mNormal;
+	
 	vec3 mAmbient;
 	vec3 mDiffuse;
 	vec3 mSpecular;
+};
+
+struct LightPoint_ {
 	vec3 mPosition;
+	float mK0, mK1, mK2;
+	
+	vec3 mAmbient;
+	vec3 mDiffuse;
+	vec3 mSpecular;
+};
+
+struct LightSpot_ {
+	vec3 mNormal;
+	vec3 mPosition;
+	float mK0, mK1, mK2;
+	float mOutCone, mInCone;
+
+	vec3 mAmbient;
+	vec3 mDiffuse;
+	vec3 mSpecular;
+};
+
+uniform struct Light_ {
+	int mDirectNum;
+	int mPointNum;
+	int mSpotNum;
+	LightDirect_ mDirects[4];
+	LightPoint_ mPoints[8];
+	LightSpot_ mSpots[8];
 } light_;
 
 uniform struct Material_ {
@@ -29,19 +59,75 @@ in V_OUT_ {
 
 out vec4 color_;
 
+vec3 CalculateDirect(LightDirect_ light, vec3 fragNormal, vec3 viewNormal)
+{
+	float diff = dot(fragNormal, light.mNormal);
+	vec3 center = (light.mNormal + viewNormal) * 0.5;
+	float spec = pow(dot(fragNormal, center), material_.mShininess);
+
+	vec3 ambient = light.mAmbient * texture(material_.mDiffuse, v_out_.mUV).rgb;
+	vec3 diffuse = light.mDiffuse * texture(material_.mDiffuse, v_out_.mUV).rgb * diff;
+	vec3 specular = light.mSpecular * texture(material_.mSpecular, v_out_.mUV).rgb * spec;
+	
+	return ambient + diffuse + specular;
+}
+
+vec3 CalculatePoint(LightPoint_ light, vec3 fragNormal, vec3 viewNormal)
+{
+	vec3 lightNormal = normalize(light.mPosition - v_out_.mMPos);
+	vec3 center = (lightNormal + viewNormal) * 0.5;
+	float diff = dot(fragNormal, lightNormal);
+	float spec = pow(dot(fragNormal, center), material_.mShininess);
+
+	vec3 ambient = light.mAmbient * texture(material_.mDiffuse, v_out_.mUV).rgb;
+	vec3 diffuse = light.mDiffuse * texture(material_.mDiffuse, v_out_.mUV).rgb * diff;
+	vec3 specular = light.mSpecular * texture(material_.mSpecular, v_out_.mUV).rgb * spec;
+
+	float distance = length(light.mPosition - v_out_.mMPos);
+	float weight = 1 / (light.mK0 + light.mK1 * distance + light.mK2 * distance * distance);
+
+	return (ambient + diffuse + specular) * weight;
+}
+
+vec3 CalculateSpot(LightSpot_ light, vec3 fragNormal, vec3 viewNormal)
+{
+	vec3 lightNormal = normalize(light.mPosition - v_out_.mMPos);
+	float fragCone = dot(-lightNormal, light.mNormal);
+	float cutWeight = clamp((fragCone - light.mOutCone) / (light.mInCone - light.mOutCone), 0, 1);
+	if (cutWeight == 0) { return vec3(0, 0, 0); }
+
+	vec3 center = (lightNormal + viewNormal) * 0.5;
+	float diff = dot(fragNormal, lightNormal);
+	float spec = pow(dot(fragNormal, center), material_.mShininess);
+
+	vec3 ambient = light.mAmbient * texture(material_.mDiffuse, v_out_.mUV).rgb;
+	vec3 diffuse = light.mDiffuse * texture(material_.mDiffuse, v_out_.mUV).rgb * diff;
+	vec3 specular = light.mSpecular * texture(material_.mSpecular, v_out_.mUV).rgb * spec;
+
+	float distance = length(light.mPosition - v_out_.mMPos);
+	float weight = 1 / (light.mK0 + light.mK1 * distance + light.mK2 * distance * distance);
+
+	return (ambient + diffuse + specular) * weight * cutWeight;
+}
+
 void main()
 {
-	vec3 fragNormal = normalize(v_out_.mNormal);
-	vec3 lightNormal = normalize(light_.mPosition - v_out_.mMPos);
-	vec3 cameraNormal = normalize(camera_pos_ - v_out_.mMPos);
+	vec3 outColor;
+	vec3 viewNormal = camera_pos_ - v_out_.mMPos;
+	for (int i = 0; i != light_.mDirectNum; ++i)
+	{
+		outColor += CalculateDirect(light_.mDirects[i], v_out_.mNormal, viewNormal);
+	}
 
-	vec3 ambient = light_.mAmbient * texture(material_.mDiffuse, v_out_.mUV).rgb;
+	for (int i = 0; i != light_.mPointNum; ++i)
+	{
+		outColor += CalculatePoint(light_.mPoints[i], v_out_.mNormal, viewNormal);
+	}
 
-	float dDotN = max(dot(lightNormal, fragNormal), 0);
-	vec3 diffuse = light_.mDiffuse * texture(material_.mDiffuse, v_out_.mUV).rgb * dDotN;
+	for (int i = 0; i != light_.mSpotNum; ++i)
+	{
+		outColor += CalculateSpot(light_.mSpots[i], v_out_.mNormal, viewNormal);
+	}
 
-	float lDotF = max(dot(normalize((lightNormal + cameraNormal) * 0.5), fragNormal), 0);
-	vec3 specular = light_.mSpecular * texture(material_.mSpecular, v_out_.mUV).rgb * pow(lDotF, material_.mShininess);
-
-	color_ = vec4(ambient + diffuse + specular, 1);
+	color_ = vec4(outColor, 1.0);
 }
