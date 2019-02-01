c8cb618e8695cd4098fc5cd13c2aa1c118c632c3
mmc <1257335379@qq.com>
2019-02-01 23:25:03 +0800

    改进RenderTarget 改进光照阴影贴图 尝试实现点阴影

--- win32/res/shadow/shadow.shader.fs
+++ win32/res/shadow/shadow.shader.fs
@@ -40,20 +40,16 @@ uniform struct Light_ {
 	LightSpot_ mSpots[4];
 	sampler2D mDirect0ShadowTex;
 	sampler2D mDirect1ShadowTex;
-	sampler2D mPoint0ShadowTex;
-	sampler2D mPoint1ShadowTex;
-	sampler2D mPoint2ShadowTex;
-	sampler2D mPoint3ShadowTex;
+	samplerCube mPoint0ShadowTex;
+	samplerCube mPoint1ShadowTex;
+	samplerCube mPoint2ShadowTex;
+	samplerCube mPoint3ShadowTex;
 	sampler2D mSpot0ShadowTex;
 	sampler2D mSpot1ShadowTex;
 	sampler2D mSpot2ShadowTex;
 	sampler2D mSpot3ShadowTex;
 	mat4 mDirect0ShadowMat;
 	mat4 mDirect1ShadowMat;
-	mat4 mPoint0ShadowMat;
-	mat4 mPoint1ShadowMat;
-	mat4 mPoint2ShadowMat;
-	mat4 mPoint3ShadowMat;
 	mat4 mSpot0ShadowMat;
 	mat4 mSpot1ShadowMat;
 	mat4 mSpot2ShadowMat;
@@ -101,6 +97,8 @@ out vec4 color_;
 		depth = texture(shadowTex, coord.xy + vec2( offset.x, -offset.y)).r; outvar += (depth == 0.0f || depth > coord.z)? 1.0f: 0.0f;	\
 		outvar = outvar / 9.0f;	}
 
+
+
 float CalculateDirectShadow(const int i)
 {
 	float shadow = 0.0f;
@@ -108,6 +106,28 @@ float CalculateDirectShadow(const int i)
 	{
 	case 0: CAL_PLANE_SHADOW(shadow, light_.mDirect0ShadowMat, light_.mDirect0ShadowTex); break;
 	case 1: CAL_PLANE_SHADOW(shadow, light_.mDirect1ShadowMat, light_.mDirect1ShadowTex); break;
+	}
+	return shadow;
+}
+
+float CalculatePointShadow(const int i)
+{
+	float shadow = 0.0f;
+	vec3 depthNormal = normalize(v_out_.mMPos - light_.mPoints[i].mPosition);
+	switch (i)
+	{
+	case 0: 
+		{
+			
+			shadow = 1;
+		}
+		break;
+	case 1:
+	break;
+	case 2:
+	break;
+	case 3:
+	break;
 	}
 	return shadow;
 }
@@ -139,19 +159,21 @@ vec3 CalculateDirect(const int i, vec3 fragNormal, vec3 viewNormal)
 	return ambient + diffuse * shadow + specular * shadow;
 }
 
-vec3 CalculatePoint(LightPoint_ light, vec3 fragNormal, vec3 viewNormal)
+vec3 CalculatePoint(const int i, vec3 fragNormal, vec3 viewNormal)
 {
-	vec3 lightNormal = normalize(light.mPosition - v_out_.mMPos);
+	
+
+	vec3 lightNormal = normalize(light_.mPoints[i].mPosition - v_out_.mMPos);
 	vec3 center = (lightNormal + viewNormal) * 0.5;
 	float diff = max(0, dot(fragNormal, lightNormal));
 	float spec = pow(max(0, dot(fragNormal, center)), material_.mShininess);
 
-	vec3 ambient = light.mAmbient * texture(material_.mDiffuse0, v_out_.mUV).rgb;
-	vec3 diffuse = light.mDiffuse * texture(material_.mDiffuse0, v_out_.mUV).rgb * diff;
-	vec3 specular = light.mSpecular * texture(material_.mSpecular0, v_out_.mUV).rgb * spec;
+	vec3 ambient = light_.mPoints[i].mAmbient * texture(material_.mDiffuse0, v_out_.mUV).rgb;
+	vec3 diffuse = light_.mPoints[i].mDiffuse * texture(material_.mDiffuse0, v_out_.mUV).rgb * diff;
+	vec3 specular = light_.mPoints[i].mSpecular * texture(material_.mSpecular0, v_out_.mUV).rgb * spec;
 
-	float distance = length(light.mPosition - v_out_.mMPos);
-	float weight = 1 / (light.mK0 + light.mK1 * distance + light.mK2 * distance * distance);
+	float distance = length(light_.mPoints[i].mPosition - v_out_.mMPos);
+	float weight = 1 / (light_.mPoints[i].mK0 + light_.mPoints[i].mK1 * distance + light_.mPoints[i].mK2 * distance * distance);
 
 	return (ambient + diffuse + specular) * weight;
 }
@@ -165,7 +187,6 @@ vec3 CalculateSpot(const int i, vec3 fragNormal, vec3 viewNormal)
 	if (cutWeight == 0) { return vec3(0, 0, 0); }
 
 	float shadow = CalculateSpotShadow(i);
-	if (shadow == 0) { return vec3(0, 0, 0); }
 
 	vec3 center = (lightNormal + viewNormal) * 0.5;
 	float diff = max(0, dot(fragNormal, lightNormal));
@@ -192,7 +213,7 @@ void main()
 
 	for (int i = 0; i != light_.mPointNum; ++i)
 	{
-		outColor += CalculatePoint(light_.mPoints[i], v_out_.mNormal, viewNormal);
+		outColor += CalculatePoint(i, v_out_.mNormal, viewNormal);
 	}
 
 	for (int i = 0; i != light_.mSpotNum; ++i)
