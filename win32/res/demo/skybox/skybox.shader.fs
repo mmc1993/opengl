bf336e6fe623826585a3fcd1beae13a6b9f39c10
mmc <1257335379@qq.com>
2019-03-01 17:38:40 +0800

    优化天空盒子

---
+++ win32/res/demo/skybox/skybox.shader.fs
@@ -0,0 +1,16 @@
+#version 410 core
+
+uniform vec3 camera_pos_;
+uniform vec3 camera_eye_;
+uniform samplerCube skybox_;
+
+in V_OUT_ {
+    vec3 mAPos;
+} v_out_;
+
+out vec4 color_;
+
+void main()
+{
+	color_ = texture(skybox_, v_out_.mAPos);
+}
