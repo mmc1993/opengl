65ede689c42ccbb34d827ea2843726a305ff654e
mmc1206 <1257335379@qq.com>
2019-04-22 17:49:05 +0800

    准备实现方向光阴影

--- win32/src/main.cpp
+++ win32/src/main.cpp
@@ -99,17 +99,17 @@ private:
 	{
 		//	坐标，环境光，漫反射，镜面反射，方向
 		const std::vector<std::array<glm::vec3, 5>> directs = {
-			//{ glm::vec3(0, 10, 10), glm::vec3(0.1f, 0.1f, 0.1f), glm::vec3(0.3f, 0.3f, 0.3f), glm::vec3(0.8f, 0.8f, 0.8f), glm::normalize(glm::vec3(0, -1, -1)) },
+			{ glm::vec3(0, 10, 10), glm::vec3(0.1f, 0.1f, 0.1f), glm::vec3(0.3f, 0.3f, 0.3f), glm::vec3(0.8f, 0.8f, 0.8f), glm::normalize(glm::vec3(0, -1, -1)) },
 		};
 
 		//	坐标，环境光，漫反射，镜面反射，衰减k0, k1, k2
 		const std::vector<std::array<glm::vec3, 5>> points = {
-			{ glm::vec3(-1.5f, 8, 3), glm::vec3(0.1f, 0.1f, 0.1f), glm::vec3(0.4f, 0.4f, 0.4f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 0.0001f, 0.00001f) },
+			//{ glm::vec3(-1.5f, 8, 3), glm::vec3(0.1f, 0.1f, 0.1f), glm::vec3(0.4f, 0.4f, 0.4f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 0.0001f, 0.00001f) },
 		};
 
 		//	坐标，环境，漫反射，镜面反射，方向，衰减k0, k1, k2，内切角，外切角
 		const std::vector<std::array<glm::vec3, 7>> spots = {
-			{ glm::vec3(-1.5f, 10, -3), glm::vec3(0.3f, 0.3f, 0.3f), glm::vec3(0.7f, 0.7f, 0.7f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0, -1, 0), glm::vec3(1.0f, 0.01f, 0.01f), glm::vec3(0.9f, 0.8f, 0.0f) },
+			//{ glm::vec3(-1.5f, 10, -3), glm::vec3(0.3f, 0.3f, 0.3f), glm::vec3(0.7f, 0.7f, 0.7f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0, -1, 0), glm::vec3(1.0f, 0.01f, 0.01f), glm::vec3(0.9f, 0.8f, 0.0f) },
 		};
 
 		for (auto & data : directs)
@@ -160,9 +160,9 @@ private:
 			object->SetParent(&mmc::mRoot);
 			_lightSpots.push_back(light);
 		}
-        //_lightDirects.at(0)->OpenShadow({ -50, 50 }, { -50, 50 }, { -10, 1000 });
-		_lightPoints.at(0)->OpenShadow(0.01f, 1000);
-        _lightSpots.at(0)->OpenShadow(0.01f, 1000);
+        _lightDirects.at(0)->OpenShadow({ -50, 50 }, { -50, 50 }, { -10, 1000 });
+		//_lightPoints.at(0)->OpenShadow(0.01f, 1000);
+        //_lightSpots.at(0)->OpenShadow(0.01f, 1000);
 	}
 
 	void OnKeyEvent(const std::any & any)
@@ -235,15 +235,15 @@ private:
 			camera->SetPos(pos);
 		}
 
-		_lightPoints.at(0)->GetOwner()->GetTransform()->Translate(-1.5f, 
-																  8 + std::cos(_pointCos) * 3, 
-																  3 + std::sin(_pointCos) * 3);
-		_pointCos += 0.1f;
+		//_lightPoints.at(0)->GetOwner()->GetTransform()->Translate(-1.5f, 
+		//														  8 + std::cos(_pointCos) * 3, 
+		//														  3 + std::sin(_pointCos) * 3);
+		//_pointCos += 0.1f;
 
-		_lightSpots.at(0)->GetOwner()->GetTransform()->Translate(4 + std::cos(_spotCos) * 3,
-																 8,
-																 0 + std::sin(_spotCos) * 5);
-		_spotCos += 0.1f;
+		//_lightSpots.at(0)->GetOwner()->GetTransform()->Translate(4 + std::cos(_spotCos) * 3,
+		//														 8,
+		//														 0 + std::sin(_spotCos) * 5);
+		//_spotCos += 0.1f;
 
 		mmc::mTimer.Add(0.016f, std::bind(&AppWindow::OnTimerUpdate, this));
 	}
