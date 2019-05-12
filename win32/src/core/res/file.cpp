5acc6efd20ac6671616490aad1a2833bceb0a3e1
mmc1206 <1257335379@qq.com>
2019-05-12 17:59:18 +0800

    加入顶点颜色属性

--- win32/src/core/res/file.cpp
+++ win32/src/core/res/file.cpp
@@ -330,6 +330,18 @@ Mesh File::LoadMesh(aiMesh * mesh, const aiScene * scene, const std::string & di
 		vertex.n.x = mesh->mNormals[i].x;
 		vertex.n.y = mesh->mNormals[i].y;
 		vertex.n.z = mesh->mNormals[i].z;
+        if (mesh->mColors[i] != nullptr)
+        {
+            //  color
+            vertex.c.r = mesh->mColors[i][0].r;
+            vertex.c.g = mesh->mColors[i][0].g;
+            vertex.c.b = mesh->mColors[i][0].b;
+            vertex.c.a = mesh->mColors[i][0].a;
+        }
+        else
+        {
+            vertex.c = glm::vec4(1, 1, 1, 1);
+        }
 		//	tan
 		vertex.tan.x = mesh->mTangents[i].x;
 		vertex.tan.y = mesh->mTangents[i].y;
@@ -339,7 +351,7 @@ Mesh File::LoadMesh(aiMesh * mesh, const aiScene * scene, const std::string & di
 		vertex.bitan.y = mesh->mBitangents[i].y;
 		vertex.bitan.z = mesh->mBitangents[i].z;
 		//	uv
-		if (mesh->mTextureCoords[0])
+		if (mesh->mTextureCoords[0] != nullptr)
 		{
 			vertex.uv.u = mesh->mTextureCoords[0][i].x;
 			vertex.uv.v = mesh->mTextureCoords[0][i].y;
