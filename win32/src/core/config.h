19bcdba42f8de2d9899602fefd4e7201447efab2
mmc1206 <1257335379@qq.com>
2019-05-23 18:26:34 +0800

    更新

---
+++ win32/src/core/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//  材质tex2d长度
+static const auto MTL_TEX2D_LEN = 8u;
+//  材质tex3d长度
+static const auto MTL_TEX3D_LEN = 8u;
+//  材质tex name长度
+static const auto MTL_TEX_NAME_LEN = 16u;
+//  MD5 长度
+static const auto MD5_LEN = 16u;
---
+++ win32/src/core/raw/gl_material.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "gl_res.h"
+#include "gl_mesh.h"
+#include "gl_program.h"
+#include "gl_texture2d.h"
+
+class GLMaterial : public GLRes {
+public:
+    struct Texture2D
+    {
+        //  材质引用的每一个纹理都必须对应一个名字, 
+        //  这个名字会对应到Shader中的uniform变量.
+        char mName[MTL_TEX_NAME_LEN];
+        const GLTexture2D * mTexture;
+    };
+
+public:
+    GLMaterial(): _glMesh(nullptr), _glProgram(nullptr)
+    {
+        memset(_glTexture2Ds, 0, sizeof(_glTexture2Ds));
+    }
+
+    void SetMesh(GLMesh * glMesh)
+    {
+        _glMesh = glMesh;
+    }
+
+    void SetProgram(GLProgram * glProgram)
+    {
+        _glProgram = glProgram;
+    }
+
+    void SetTexture2D(const GLTexture2D * glTexture2D, const std::string & name, uint i)
+    {
+        _glTexture2Ds[i].mTexture = glTexture2D;
+
+        memcpy(_glTexture2Ds[i].mName, name.c_str(), name.size());
+    }
+
+    const GLMesh * GetMesh() const
+    {
+        return _glMesh;
+    }
+
+    const GLProgram * GetProgram() const
+    {
+        return _glProgram;
+    }
+
+    const Texture2D * GetTexture2Ds(uint i) const
+    {
+        if (i < MTL_TEX2D_LEN)
+        {
+            return &_glTexture2Ds[i];
+        }
+        return nullptr;
+    }
+
+private:
+    const GLMesh      * _glMesh;
+    const GLProgram   * _glProgram;
+    Texture2D           _glTexture2Ds[MTL_TEX2D_LEN];
+};
