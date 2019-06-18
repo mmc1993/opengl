65c939f59837242760bab30a701687afa1b48259
mmc1206 <1257335379@qq.com>
2019-06-18 14:28:41 +0800

    更新

--- win32/src/core/tools/serialize_tool.h
+++ win32/src/core/tools/serialize_tool.h
@@ -1,7 +1,7 @@
 #pragma once
 
 //  POD
-template <class T, typename std::enable_if_t<std::is_trivial_v<T>, int> N = 0>
+template <class T, typename std::enable_if_t<std::is_trivially_copyable_v<T>, int> N = 0>
 void Serialize(std::ostream & os, const T & val)
 {
     os.write((const char *)&val, sizeof(T));
@@ -11,9 +11,28 @@ void Serialize(std::ostream & os, const T & val)
 template <class T, typename std::enable_if_t<
     std::is_same_v<typename T::iterator, decltype(std::declval<T>().begin())> &&
     std::is_same_v<typename T::iterator, decltype(std::declval<T>().end())>, int> N = 0>
-    void Serialize(std::ostream & os, const T & val)
+void Serialize(std::ostream & os, const T & val)
 {
     unsigned int size = val.size();
     os.write((const char *)&size, sizeof(size));
     for (auto & v : val) { Serialize(os, v); }
+}
+
+//  POD
+template <class T, typename std::enable_if_t<std::is_trivially_copyable_v<T>, int> N = 0>
+void Deserialize(std::istream & is, T & val)
+{
+    is.read((char *)&val, sizeof(T));
+}
+
+//  ÈÝÆ÷
+template <class T, typename std::enable_if_t<
+    std::is_same_v<typename T::iterator, decltype(std::declval<T>().begin())> &&
+    std::is_same_v<typename T::iterator, decltype(std::declval<T>().end())>, int> N = 0>
+void Deserialize(std::istream & is, T & val)
+{
+    unsigned int size = 0;
+    is.read((char *)&size, sizeof(unsigned int));
+    val.resize(size);
+    for (auto & v : val) { Deserialize(is, v); }
 }
