d704a08a5f6835bd35821e3e5409dae52c777721
mmc1206 <1257335379@qq.com>
2019-06-18 11:12:32 +0800

    改进序列化接口

--- win32/src/core/tools/serialize_tool.h
+++ win32/src/core/tools/serialize_tool.h
@@ -1,39 +1,19 @@
 #pragma once
 
-#ifndef SERIALIZE_TOOL
-
-#define SERIALIZE_TOOL
-
-#define SERIALIZE_BEG                                                               void Serialize(std::ofstream & os) {
-#define SERIALIZE_END                                                               }
-
-#define DESERIALIZE_BEG                                                             void Deserialize(std::ifstream & is) {
-#define DESERIALIZE_END                                                             }
-
-#define SERIALIZE_POD(  member)                                                     os.write((char *)&member, sizeof(member));
-#define DESERIALIZE_POD(member)                                                     is.read( (char *)&member, sizeof(member));
-
-#define SERIALIZE_POD_ARRAY(member)    {                                            \
-    auto size = member.size();                                                      \
-    os.write((char *)&size, sizeof(uint));                                          \
-    os.write((char *)member.data(), size * sizeof(decltype(member)::value_type));   \
-}
-#define DESERIALIZE_POD_ARRAY(member)  {                                            \
-    uint size = 0;                                                                  \
-    is.read((char *)&size, sizeof(uint));                                           \
-    member.resize(size);                                                            \
-    is.read((char *)member.data(), size * sizeof(decltype(member)::value_type));    \
+//  POD
+template <class T, typename std::enable_if_t<std::is_trivial_v<T>, int> N = 0>
+void Serialize(std::ostream & os, const T & val)
+{
+    os.write((const char *)&val, sizeof(T));
 }
 
-#define SERIALIZE_ARRAY(member) {                                                   \
-    uint size = member.size();                                                      \
-    os.write((char *)&size, sizeof(uint));                                          \
-    for (auto & ele: member) { ele.Serialize(os); }                                 \
+//  ÈÝÆ÷
+template <class T, typename std::enable_if_t<
+    std::is_same_v<typename T::iterator, decltype(std::declval<T>().begin())> &&
+    std::is_same_v<typename T::iterator, decltype(std::declval<T>().end())>, int> N = 0>
+    void Serialize(std::ostream & os, const T & val)
+{
+    unsigned int size = val.size();
+    os.write((const char *)&size, sizeof(size));
+    for (auto & v : val) { Serialize(os, v); }
 }
-#define DESERIALIZE_ARRAY(member) {                                                 \
-    uint size = 0;                                                                  \
-    is.read((char *)&size, sizeof(uint));                                           \
-    member.resize(size);                                                            \
-    for (auto & ele: member) { ele.Deserialize(is); }                               \
-}
-#endif
